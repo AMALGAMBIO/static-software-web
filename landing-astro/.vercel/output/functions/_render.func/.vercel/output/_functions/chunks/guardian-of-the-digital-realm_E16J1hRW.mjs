867704938ebbe9ff2d6f72fcee87fe674a28a98a
Tristan NGUYEN <trungupdate@gmail.com>
2024-10-24 09:30:02 +0700

    build 1

---
+++ landing-astro/.vercel/output/functions/_render.func/.vercel/output/_functions/chunks/guardian-of-the-digital-realm_E16J1hRW.mjs
@@ -0,0 +1,9 @@
+async function getMod() {
+						return import('./guardian-of-the-digital-realm_6H9VvsPK.mjs');
+					}
+					const collectedLinks = [];
+					const collectedStyles = [];
+					const collectedScripts = [];
+					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
+
+export { defaultMod as default };
